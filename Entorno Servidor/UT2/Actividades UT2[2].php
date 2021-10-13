72278befd32488c3910216feaeb84df0fd160f02
Fernando Santamaría <fsantagestiones@gmail.com>
2021-10-13 14:17:38 +0200

    Actividades. Actualizado (13/10/2021)

--- Entorno Servidor/UT2/Actividades UT2[2].php
+++ Entorno Servidor/UT2/Actividades UT2[2].php
@@ -76,12 +76,117 @@
     <br>
     <h2>Ejercicio 4</h2>
     <?php
-        $matriz=array('parte1'=>array(1,0),'parte2'=>array(0,1));
-           /*  /* if($matriz['parte1'[0]]=$matriz['parte2'[1]]) && $matriz['parte1'[0]]=1 && $matriz['parte2'[1]]=1){
-                echo "La matriz es identidad";
-            }else { 
-                echo "La matriz NO es identidad";
-            } */
+        //Creación y visualización de la matriz
+        $tabla=array(array(0,1),array(1,0));
+        for($i=0;$i<count($tabla);$i++){
+            for($j=0;$j<count($tabla[$i]);$j++){
+                echo $tabla[$i][$j]," ";          
+            }
+            echo "<br>";
+        }
+        //Tratamiento Matriz identidad
+
+        $identidad=true;
+        for($i=0;$i<count($tabla) && $identidad;$i++){
+            for($j=0;$j<count($tabla[$i]) && $identidad;$j++){
+                if($i==$j && $tabla[$i][$j]!=1){
+                    $identidad=false;
+                }else if ($i!=$j && $tabla[$i][$j]!=0){
+                    $identidad=false;
+                }
+            }
+        }
+        if($identidad){
+            echo "La matriz es identidad";
+        }else{
+            echo "La matriz no es identidad";
+        }
+    ?>
+    <br>
+    <h2>Ejercicio 5</h2>
+    <?php
+        $tabla=array(array(-8,1,4),array(1,7,4),array(3,4,9));
+        
+        for($i=0;$i<count($tabla);$i++){
+            for($j=0;$j<count($tabla[$i]);$j++){
+                echo $tabla[$i][$j]," ";
+            }
+            echo "<br>";
+        }
+
+        $simétrica=true;
+        for($i=0;$i<count($tabla) && $simétrica;$i++){
+            for($j=0;$j<count($tabla[$i]) && $simétrica;$j++){
+                if($tabla[$i][$j]!=$tabla[$j][$i]){
+                    $simétrica=false;
+                }
+            }
+        }
+        if($simétrica==true){
+            echo "La matriz es simétrica";
+        }else{
+            echo "La matriz no es simétrica";
+        }
+        echo "<br>";
+        
+        $suma=0;
+        $matriz=array();
+            for($j=0;$j<count($tabla[$i]);$j++){
+                for($i=0;$i<=2;$i++){
+                    $suma+=$tabla[$i][$j];
+                }
+                $matriz[]=$suma;
+                $suma=0;
+                $i=0;
+            }
+        echo "Array suma: <br>";
+        for($i=0;$i<count($matriz);$i++){
+            echo $matriz[$i],"<br>";
+        }
+
+        $búsqueda = 9;
+        $mensaje = "El número $búsqueda se encuentra en las coordenadas ";
+        for($i=0;$i<count($tabla);$i++){
+            for($j=0;$j<count($tabla[$i]);$j++){
+                if($tabla[$i][$j]==$búsqueda){
+                    $coordx=$i+1;
+                    $coordy=$j+1;
+                }
+            }
+        }
+        echo "Valor: ", $búsqueda,
+        "<br>",
+        "Ubicación: (",$coordx,",",$coordy,") <br>";
+    ?>
+    <br>
+    <h2>Ejercicio 6</h2>
+    <?php
+    
+    ?>
+    <br>
+    <h2>Ejercicio 7</h2>
+    <?php
+    
+    ?>
+    <br>
+    <h2>Ejercicio 8</h2>
+    <?php
+    
+    ?>
+    <br>
+    <h2>Ejercicio 9</h2>
+    <?php
+    
+    ?>
+    <br>
+    <h2>Ejercicio 10</h2>
+    <?php
+    
+    ?>
+    <br>
+    <h2>Ejercicio 11</h2>
+    <?php
+    
     ?>
 </body>
 </html>
