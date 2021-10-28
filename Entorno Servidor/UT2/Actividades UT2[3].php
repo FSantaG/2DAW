dda0a213b30875ddd44eb35c8fd4af582bf74706
FSantaG <fsantagestiones@gmail.com>
2021-10-28 11:10:23 +0200

    Ejercicio 22 y 13 PHP

--- Entorno Servidor/UT2/Actividades UT2[3].php
+++ Entorno Servidor/UT2/Actividades UT2[3].php
@@ -155,7 +155,21 @@
     <br>
     <h1>Ejercicio 13 </h1>
     <?php
+    function traducir($frase_traducir){
+        $palabra_sin_primera_letra="";
+        $primera_letra="";
+        $frase_traducida="";
+        $frase_array=explode(" ", $frase_traducir);
 
+        foreach($frase_array as $frase){
+            $primera_letra=$frase[0];
+            $palabra_sin_primera_letra=substr($frase, 1);
+            $frase_traducida.= $palabra_sin_primera_letra.$primera_letra."um ";
+        }
+        return $frase_traducida;
+    }
+    $frase="una imagen vale más que mil palabras";
+    echo "'$frase' en macarrónico es: <b>",traducir($frase),"</b>";
     ?>
     <br>
     <h1>Ejercicio 14 </h1>
@@ -200,7 +214,72 @@
     <br>
     <h1>Ejercicio 22 </h1>
     <?php
+    //Función convencional de usuario
+    function función_clásica($cadenas, $subcad='ma'){
+        $cont=0; $ok=false;
+        $n=func_num_args();
+        if($n==2){
+            $ok=true;
+        }
+        for($i=0; $i<count($cadenas); $i++){
+            if(stripos($cadenas[$i],$subcad)!==false){
+                $cont++;
+            }
+        }
+        if($ok){
+            echo "La subcadena '$subcad' aparece $cont veces en el array de string <br>";
+        }else{
+            echo "No se pasó ninguna subcadena. La subcadena 'ma' aparece $cont veces en el string <br>";
+        }
+    }
+    echo "USANDO UNA FUNCIÓN CLÁSICA <br>";
+    $palabras=array("lunes", "martes", "miercoles");
+    función_clásica($palabras, "ES");
+    echo "<br>";
+    función_clásica($palabras, "s");
+    echo "<br>";
+    función_clásica($palabras);
+    echo "<br>";
 
+    //Variable de función
+    echo "USANDO UNA VARIABLE DE FUNCIÓN <br>";
+    $var_func='función_clásica';
+    $var_func($palabras);
+    echo("<br>");
+    $var_func($palabras, "s");
+    echo("<br>");
+
+    //funcion anónima y callback
+    echo "USANDO UNA VARIABLE CALLBACK <br>";
+    $var2_func=function($cadenas, $subcad="ma"){
+        $cont=0;
+        $ok=false;
+        $n=func_num_args();
+        if($n==2){
+            $ok=true;
+        }
+        for($i=0; $i<count($cadenas); $i++){
+            if(stripos($cadenas[$i],$subcad)!== false){ 
+                $cont++;
+            }
+        }
+        if($ok){
+            echo "La subcadena '$subcad' aparece $cont veces en el array de string <br>";
+        }else{
+            echo "No se pasó ninguna subcadena. La subcadena 'ma' aparece $cont veces en el string <br>";
+        }
+    };
+
+    //Es necesario el ; pues de trata de una sentencia de asignación.
+
+    function probar($var){
+        $palabras=array("lunes", "martes", "miercoles");
+        $var($palabras);
+        echo "<br>";
+        $var($palabras, "r");
+    }
+
+    probar($var2_func);
     ?>
     <br>
     <h1>Ejercicio 23 </h1>
