b0ee7e682b37facc064053f30f3ed50057282f28
FSantaG <fsantagestiones@gmail.com>
2021-12-22 09:46:37 +0100

    Ayuda ejercicios UT7

---
+++ Entorno Cliente/Ejercicios de Práctica/Ejercicios UT7 - Fernando Santamaría García/ayuda-eventos.js
@@ -0,0 +1,28 @@
+/* EJERCICIO 4 - ZONAS PULSAR
+==========================
+Posición del ratón respecto al navegador:
+   event.clientX
+   event.clientY
+Tamaño de la ventana del navegador:
+   document.documentElement.clientWidth
+   document.documentElement.clientHeight
+
+
+
+EJERCICIO 5 - INFORMACIÓN EVENTOS
+=================================
+Evento que detecta el movimiento del ratón:
+   mousemove
+Posición del ratón respecto al navegador:
+   event.clientX
+   event.clientY
+Posición del ratón respecto a la pantalla:
+   event.screenX
+   event.screenY
+
+Evento que detecta que se ha pulsado una techa:
+   keydown
+Valor de la tecla pulsada:
+   event.key
+Código de la tecla pulsada:
+   event.code */
