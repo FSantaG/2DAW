6c04b7e1c45c8d37bac994af95f6fa28f474a6df
FSantaG <fsantagestiones@gmail.com>
2022-01-12 13:10:59 +0100

    Ejercicio 9 PHP corregido

--- Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 9/_core_SID_href.php
+++ Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 9/_core_SID_href.php
@@ -3,4 +3,4 @@ session_start();
 require_once("./codigo_pag_580.php");
 $_SESSION["nombre"]="Manolo";
 ?>
-<a href="cons_SID2_href_php">Página 2 </a>
+<a href="<?=url('cons_SID2_href.php')?>">Página 2 </a>
--- Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 9/codigo_pag_580.php
+++ Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 9/codigo_pag_580.php
@@ -1,11 +1,18 @@
 <?php
 function url($url){
+    //Si la directiva de configuración session.use_trans_sid
+    //está en 0(no se inicializa ninguna transmisi´ñon automñaticca por URL) y
+    //si SID no está vacío (el equipo ha rechazado la cookie), entonces
+    //debemos gestionar la transmisión
     if((get_cfg_var('session.use_trans_sid')==0) and (SID !='')){
+        //Agregar la constante  SID detrás de la URL con un "?"
+        //si todavía no hay ningún parámetro, o con un & en
+        //caso contrario
         $url.=((strpos($url,'?')===FALSE)?'?':'&').SID;
     }
     return $url;
 }
-session_start();
-echo url("cons_SID_href.php"),"<br/>";
-echo url("cons_SID2_href.php?nombre=Manolo"),"<br/>";
+// session_start();
+// echo url("cons_SID_href.php"),"<br/>";
+// echo url("cons_SID2_href.php?nombre=Manolo"),"<br/>";
 ?>
