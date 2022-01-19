7a1b7dca81245c86c058e30d9a3a654fdf38abcb
FSantaG <fsantagestiones@gmail.com>
2022-01-19 08:58:06 +0100

    Ejercicio 10 PHP (Carrito de la compra) terminado

---
+++ Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 10/finApp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gracias por usar nuestra aplicación</title>
+</head>
+<body>
+    <h1>Gracias por su visita</h1>
+    Su carrito se ha guardado hasta que decidas regresar. Recuerde que estaos abierto todos los días de la semana, las 24 horas del día
+</body>
+</html>
--- Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 10/resumen.php
+++ Entorno Servidor/UT5/Cookies/Actividades UT5[1]/Ejercicio 10/resumen.php
@@ -1,11 +1,32 @@
 <?php
-    $data = json_decode($_COOKIE['resumen']);
-    echo count($data);
-    foreach($data as $producto){
+    $datosLista = json_decode($_COOKIE['resumen']);
+    $contador = 1;
+    $preciototal = 0;
+    $precio = 0;
+    unset($_COOKIE['resumen']);
+    //echo count($data);
+    foreach($datosLista as $dato){
         //TODO
         //Hacer la cosa de mierda para comprobar los precios
-        echo $producto."<br>";
+        if($contador==1){
+            $producto = mb_strtolower($dato);
+            echo "Producto: ".$producto." ";
+            if($producto == "manzanas"){
+                $precio = 1.80;
+            }else if($producto == "peras"){
+                $precio = 1.40;
+            }else if($producto == "plátanos"){
+                $precio = 1.50;
+            }
+            $contador++;
+        }else{
+            $cantidad = $dato;
+            echo "Cantidad: ".$cantidad." Precio: ".$precio*$cantidad."<br/>";
+            $preciototal += $precio*$cantidad;
+            $contador=1;
+        }
     }
+    echo "Total a pagar: ".$preciototal."<br/>";
 ?>
 
 <!DOCTYPE html>
