1485f33877625432ad0b20f4a9a9c830f56a1479
FSantaG <fsantagestiones@gmail.com>
2022-02-22 14:19:55 +0100

    Página web WIP

---
+++ Interfaces Web/UT6/Página web/js/menuHamburguesa.js
@@ -0,0 +1,8 @@
+function mostrarMenu() {
+  var x = document.getElementById("navegacion");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
