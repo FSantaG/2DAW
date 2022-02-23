2a1d6f37cc0f3a26cb19cdcd038b884543b0af1c
FSantaG <fsantagestiones@gmail.com>
2022-02-23 12:14:21 +0100

    Página Web

--- Interfaces Web/UT6/Página web/js/menuHamburguesa.js
+++ Interfaces Web/UT6/Página web/js/menuHamburguesa.js
@@ -1,8 +1,8 @@
 function mostrarMenu() {
-  var x = document.getElementById("navegacion");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var menu = document.getElementById("navegacion");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
   } else {
-    x.style.display = "block";
+    menu.style.display = "block";
   }
 }
